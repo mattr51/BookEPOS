2588656112dadfcc436b8b4980413d96270ac8b1
mattr51 <51mattr@gmail.com>
2016-03-31 23:04:48 +0100

    Initial commit

---
+++ app/src/main/java/uk/ac/qub/bookepos/BookDetailActivity.java
@@ -0,0 +1,103 @@
+package uk.ac.qub.bookepos;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+import uk.ac.qub.bookepos.Book;
+import uk.ac.qub.bookepos.BookClient;
+import uk.ac.qub.bookepos.BookListActivity;
+
+public class BookDetailActivity extends AppCompatActivity {
+    private ImageView ivBookCover;
+    private TextView tvTitle;
+    private TextView tvAuthor;
+    private TextView tvPublisher;
+    private TextView tvPageCount;
+    private BookClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_detail);
+        // Fetch views
+        ivBookCover = (ImageView) findViewById(R.id.ivBookCover);
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        tvAuthor = (TextView) findViewById(R.id.tvAuthor);
+        tvPublisher = (TextView) findViewById(R.id.tvPublisher);
+        tvPageCount = (TextView) findViewById(R.id.tvPageCount);
+        // Use the book to populate the data into our views
+        Book book = (Book) getIntent().getSerializableExtra(BookListActivity.BOOK_DETAIL_KEY);
+        loadBook(book);
+    }
+
+    // Populate data for the book
+    private void loadBook(Book book) {
+        //change activity title
+        this.setTitle(book.getTitle());
+        // Populate data
+        Picasso.with(this).load(Uri.parse(book.getLargeCoverUrl())).error(R.mipmap.ic_launcher).into(ivBookCover);
+        tvTitle.setText(book.getTitle());
+        tvAuthor.setText(book.getAuthor());
+        // fetch extra book data from books API
+        client = new BookClient();
+        client.getExtraBookDetails(book.getOpenLibraryId(), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    if (response.has("publishers")) {
+                        // display comma separated list of publishers
+                        final JSONArray publisher = response.getJSONArray("publishers");
+                        final int numPublishers = publisher.length();
+                        final String[] publishers = new String[numPublishers];
+                        for (int i = 0; i < numPublishers; ++i) {
+                            publishers[i] = publisher.getString(i);
+                        }
+                        tvPublisher.setText(TextUtils.join(", ", publishers));
+                    }
+                    if (response.has("number_of_pages")) {
+                        tvPageCount.setText(Integer.toString(response.getInt("number_of_pages")) + " pages");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_book_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+      /*  //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+*/
+        return super.onOptionsItemSelected(item);
+    }
+}
