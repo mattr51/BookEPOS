bfa0463aa5aa08fd3a3c269d98174d910f0c8120
mattr51 <51mattr@gmail.com>
2016-04-11 22:20:36 +0100

    Fixed merge conflict

--- app/src/main/java/uk/ac/qub/bookepos/BookSearchFragment.java
+++ app/src/main/java/uk/ac/qub/bookepos/BookSearchFragment.java
@@ -2,15 +2,15 @@ package uk.ac.qub.bookepos;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SearchView;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 
@@ -18,14 +18,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import cz.msebera.android.httpclient.Header;
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import cz.msebera.android.httpclient.Header;
 
 /**
  * Created by Matt Ralphson
  */
-public class BookSearchActivity extends AppCompatActivity {
+public class BookSearchFragment extends Fragment {
     private ListView lvBooks;
     private BookAdapter bookAdapter;
     private ProgressBar progress;
@@ -33,25 +34,34 @@ public class BookSearchActivity extends AppCompatActivity {
     public static final String BOOK_DETAIL_KEY = "book";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_search);
-        lvBooks = (ListView) findViewById(R.id.lvBooks);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_book_search, container, false);
+        lvBooks = (ListView) view.findViewById(R.id.lvBooks);
         ArrayList<Book> aBooks = new ArrayList<Book>();
-        Book testBook = new Book("Test title", "Test author", 10);
+        Book testBook = new Book(1, "Test title", "Test author", 10);
         aBooks.add(testBook);
-        bookAdapter = new BookAdapter(this, aBooks);
+        bookAdapter = new BookAdapter(getContext(), aBooks);
         lvBooks.setAdapter(bookAdapter);
-        progress = (ProgressBar) findViewById(R.id.progress);
+        progress = (ProgressBar) view.findViewById(R.id.progress);
+
+        Button doSearchButton = (Button) view.findViewById(R.id.do_search_button);
+        doSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView searchTextView = (TextView)getView().findViewById(R.id.search_terms_text);
+                doSearch(searchTextView.getText().toString());
+            }
+        });
+
         setupBookSelectedListener();
+        return view;
     }
 
-    private void DoSearch(String searchTerms) {
-        InventoryApiEndPoint inventoryApiEndPoint = new InventoryApiEndPoint();
+    private void doSearch(String searchTerms) {
+        InventoryApiEndPoint inventoryApiEndPoint = new InventoryApiEndPoint(bookAdapter);
         HashMap<String, String> urlParams = new HashMap<>();
         urlParams.put("searchTerms", searchTerms);
         inventoryApiEndPoint.execute(urlParams);
-        bookAdapter = new BookAdapter(this, inventoryApiEndPoint.getBooks());
     }
 
     public void setupBookSelectedListener() {
@@ -59,7 +69,7 @@ public class BookSearchActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // Launch the detail view passing book as an extra
-                Intent intent = new Intent(BookSearchActivity.this, BookDetailActivity.class);
+                Intent intent = new Intent(getContext(), BookDetailActivity.class);
                 intent.putExtra(BOOK_DETAIL_KEY, bookAdapter.getItem(position));
                 startActivity(intent);
             }
@@ -103,6 +113,43 @@ public class BookSearchActivity extends AppCompatActivity {
         });
     }
 
+    // Executes an API call to the OpenLibrary search endpoint, parses the results
+    // Converts them into an array of book objects and adds them to the adapter
+    private void fetchByISBN(String isbn) {
+        client = new BookClient();
+        progress.setVisibility(ProgressBar.VISIBLE);
+        client.getBooks(isbn, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    progress.setVisibility(ProgressBar.GONE);
+                    JSONArray docs = null;
+                    if (response != null) {
+                        // Get the docs json array
+                        docs = response.getJSONArray("docs");
+                        // Parse json array into array of model objects
+                        final ArrayList<Book> books = Book.fromJson(docs);
+                        // Remove all books from the adapter
+                        bookAdapter.clear();
+                        // Load model objects into the adapter
+                        for (Book book : books) {
+                            bookAdapter.add(book); // add book through the adapter
+                        }
+                        bookAdapter.notifyDataSetChanged();
+                    }
+                } catch (JSONException e) {
+                    // Invalid JSON format, show appropriate error.
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                progress.setVisibility(ProgressBar.GONE);
+            }
+        });
+    }
+
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -120,7 +167,7 @@ public class BookSearchActivity extends AppCompatActivity {
                 searchView.setIconified(true);
                 searchItem.collapseActionView();
                 // Set activity title to search query
-                BookSearchActivity.this.setTitle(query);
+                BookSearchFragment.this.setTitle(query);
                 return true;
             }
 
@@ -130,5 +177,5 @@ public class BookSearchActivity extends AppCompatActivity {
             }
         });
         return true;
-    }
+    }*/
 }
